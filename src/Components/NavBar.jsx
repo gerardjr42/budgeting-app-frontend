d04a41f45ff959d256aba13af771f99300cbb99b
gerardjr42 <ggarciajr12@gmail.com>
2024-07-09 21:35:14 -0400

    Add cursor pointers to all text with links

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,11 +3,14 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <div className="flex w-full bg-teal-400 p-8">
-      <Link to="/transactions" className="justify-start self-center text-6xl">
+      <Link
+        to="/transactions"
+        className="cursor-pointer justify-start self-center text-6xl"
+      >
         Budget App
       </Link>
       <div className="flex flex-1 justify-end self-center">
-        <button className="border border-gray-400 px-8 py-3 text-xs hover:bg-blue-500">
+        <button className="cursor-pointer border border-gray-400 px-8 py-3 text-xs hover:bg-blue-500">
           <Link to="/transactions/new">NEW TRANSACTION</Link>
         </button>
       </div>
